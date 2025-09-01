1911faadb2b384cdcf3ee2b3527a3e1de1b91909
ahernandezm1001 <152576027+ahernandezm1001@users.noreply.github.com>
2025-09-01 15:33:15 -0600

    Create calculadora_colaborativa.c
    "Esqueleto base de la calculadora"

---
+++ calculadora_colaborativa.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("CALCULADORA COLABORATIVA\n\n");
+    printf("1. SUMA:\n");
+    printf("2. RESTA:\n");
+    printf("3. MULTIPLICACION:\n");
+    printf("4. DIVISION:\n");
+    int opcion;
+    printf("Elige una opcion: ");
+    scanf("%d", &opcion);
+    float num1, num2, resultado;
+    printf("Ingresa el primer numero: ");
+    scanf("%f", &num1);
+    printf("Ingresa el segundo numero: ");
+    scanf("%f", &num2);
+    switch(opcion){
+        case 1:
+            
+        case 2:
+            
+        case 3:
+            
+        case 4:
+        
+        default:
+            printf("Opcion no valida.\n");
+    }
+    return 0;
+}
